1adf4291d78cde3dc137cb6197412284371f17ea
Ercan Duman <ercan.duman@macellan.net>
2021-09-21 16:07:01 +0300

    AppFirebaseMessagingService.kt created to handle FCM token related operations.

---
+++ app/src/main/java/ercanduman/fcmmessagingdemo/data/service/AppFirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package ercanduman.fcmmessagingdemo.data.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+/**
+ * Handles Firebase FCM operations such as receiving new token or sending tokens to backend server.
+ *
+ * @author ercanduman
+ * @since  21.09.2021
+ */
+class AppFirebaseMessagingService : FirebaseMessagingService() {
+}
