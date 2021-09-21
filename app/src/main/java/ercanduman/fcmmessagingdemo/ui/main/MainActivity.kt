6696cc9d7cbdc42f147fa7f73cffa46b4a61b909
Ercan Duman <ercan.duman@macellan.net>
2021-09-21 16:47:00 +0300

    ui related items moved to its package

--- app/src/main/java/ercanduman/fcmmessagingdemo/ui/main/MainActivity.kt
+++ app/src/main/java/ercanduman/fcmmessagingdemo/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package ercanduman.fcmmessagingdemo
+package ercanduman.fcmmessagingdemo.ui.main
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import ercanduman.fcmmessagingdemo.R
 import ercanduman.fcmmessagingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
